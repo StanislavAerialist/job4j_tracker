5502ebd9f166c9c8142dc710ac38aabc1bffc358
StanislavAerialist <moonlight_stas@mail.ru>
2022-08-11 08:29:04 +0500

    6. Метод замены заявки. Tracker.replace [#211748 #330668]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -47,13 +47,13 @@ public class Tracker {
     }
 
     public boolean replace(int id, Item item) {
-            int index = indexOf(id);
-            boolean rsl = index != -1;
-            if (rsl) {
-                item.setId(id);
-                items[index] = item;
-            }
-            return rsl;
+        int index = indexOf(id);
+        boolean rsl = index != -1;
+        if (rsl) {
+            item.setId(id);
+            items[index] = item;
+        }
+        return rsl;
     }
 
     public boolean delete(int id) {
