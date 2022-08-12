4faa8ec720d815dfc4e8d9f07cd8d1c8e24e6cb5
StanislavAerialist <moonlight_stas@mail.ru>
2022-08-12 15:32:44 +0500

    5.1. Тестирование. Подготовка данных. [#182960 #331359]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -38,5 +38,6 @@ public class StartUITest {
         StartUI.deleteItem(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
         assertThat(deleted).isNull();
+
     }
 }
