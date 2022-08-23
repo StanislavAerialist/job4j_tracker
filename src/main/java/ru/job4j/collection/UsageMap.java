33838956a2a9c8afaf2701cd9e6c702117ac4a21
StanislavAerialist <moonlight_stas@mail.ru>
2022-08-23 09:33:30 +0500

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("bbj@mail.ru", "Barb Barby Johnson");
+        map.put("qwerty@gmail.ru", "Qwert Qwerty Q");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + " = " + value);
+        }
+    }
+}
