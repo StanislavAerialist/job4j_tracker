dcba6e7b8cb19fa41359e73cb05efe0c925caa31
StanislavAerialist <moonlight_stas@mail.ru>
2022-08-25 14:39:40 +0500

    2. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -1,7 +1,6 @@
 package ru.job4j.collection;
 
 import java.util.Comparator;
-
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
