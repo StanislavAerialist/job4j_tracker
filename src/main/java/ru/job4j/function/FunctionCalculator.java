5d84b86aae0e29d072ebe858523e39bd8eb7a522
StanislavAerialist <moonlight_stas@mail.ru>
2022-08-30 08:20:50 +0500

    5. Подсчет функции в диапазоне. [#24251 #337447]

--- src/main/java/ru/job4j/function/FunctionCalculator.java
+++ src/main/java/ru/job4j/function/FunctionCalculator.java
@@ -3,20 +3,12 @@ package ru.job4j.function;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 public class FunctionCalculator {
     public List<Double> diapason(int start, int end, Function<Double, Double> func) {
-        Supplier<List<Double>> sup = ArrayList::new;
-        List<Double> rsl = sup.get();
-        Predicate<Double> cond = aDouble -> aDouble < end;
-        Double con = (double) start;
-        Function<Double, Boolean> addf = rsl::add;
-        while (cond.test(con)) {
-            Double num = func.apply(con);
-            addf.apply(num);
-            con++;
+        List<Double> rsl = new ArrayList<>();
+        for (int i = start; i < end; i++) {
+            rsl.add(func.apply((double) i));
         }
         return rsl;
     }
--- src/test/java/ru/job4j/function/FunctionCalculatorTest.java
+++ src/test/java/ru/job4j/function/FunctionCalculatorTest.java
@@ -27,8 +27,8 @@ public class FunctionCalculatorTest {
     @Test
     public void whenExponentialFunctionThenExponentialResults() {
         FunctionCalculator function = new FunctionCalculator();
-        List<Double> result = function.diapason(1, 4, x -> Math.pow(x, x) + 1);
-        List<Double> expected = Arrays.asList(2D, 5D, 28D);
+        List<Double> result = function.diapason(1, 4, x -> Math.pow(2, x) + 1);
+        List<Double> expected = Arrays.asList(3D, 5D, 9D);
         assertThat(result).containsAll(expected);
     }
 }
