43911759bb3d10382311883946395b0001f5d4cb
StanislavAerialist <moonlight_stas@mail.ru>
2023-04-25 21:13:08 +0500

    1. Интеграционное тестирование с Hibernate

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -5,24 +5,66 @@ import lombok.*;
 import javax.persistence.*;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "items")
 @Data
-@RequiredArgsConstructor
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Item {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @EqualsAndHashCode.Include
-    private Integer id;
-
-    @NonNull
-    @EqualsAndHashCode.Include
+    private int id;
     private String name;
-
     private LocalDateTime created = LocalDateTime.now();
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+    @ManyToMany
+    @JoinTable(
+            name = "participates",
+            joinColumns = { @JoinColumn(name = "item_id") },
+            inverseJoinColumns = { @JoinColumn(name = "user_id") }
+    )
+    private List<User> participates = new ArrayList<>();
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Item(int id, String name, LocalDateTime created) {
+        this.id = id;
+        this.name = name;
+        this.created = created;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("id: %s, name: %s, created: %s", id, name, FORMATTER.format(created));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return name.equals(item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
---
+++ src/test/java/ru/job4j/tracker/HbmTrackerTest.java
@@ -0,0 +1,93 @@
+package ru.job4j.tracker;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.After;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+
+class HbmTrackerTest {
+
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+    private final SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+    @After
+    public void wipeTable() {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery("DELETE FROM Item").executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+    }
+
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() throws Exception {
+        try (var tracker = new HbmTracker()) {
+            Item item = new Item();
+            item.setName("test1");
+            tracker.add(item);
+            Item result = tracker.findById(item.getId());
+            assertThat(result.getName(), is(item.getName()));
+        }
+    }
+
+    @Test
+    public void whenDeleteItemThenNull() throws Exception {
+        try (var tracker = new HbmTracker()) {
+            Item item = new Item("item");
+            tracker.add(item);
+            boolean result = tracker.delete(item.getId());
+            assertThat(result, is(true));
+            assertThat(tracker.findById(item.getId()), is(nullValue()));
+        }
+    }
+
+    @Test
+    public void whenReplaceItem1ThenItem2() throws Exception {
+        try (var tracker = new HbmTracker()) {
+            Item item1 = new Item("item1");
+            Item item2 = new Item("item2");
+            tracker.add(item1);
+            boolean result = tracker.replace(item1.getId(), item2);
+            assertThat(result, is(true));
+            assertThat(tracker.findById(item1.getId()).getName(), is(item2.getName()));
+        }
+    }
+
+    @Test
+    public void whenFindAllThenList() throws Exception {
+        try (var tracker = new HbmTracker()) {
+            Item item1 = new Item("item1");
+            Item item2 = new Item("item2");
+            tracker.add(item1);
+            tracker.add(item2);
+            List<Item> result = tracker.findAll();
+            assertThat(result, is(List.of(item1, item2)));
+        }
+    }
+
+    @Test
+    public void whenFindByNameThenList() throws Exception {
+        try (var tracker = new HbmTracker()) {
+            Item item1 = new Item("item1");
+            Item item2 = new Item("item2");
+            tracker.add(item1);
+            tracker.add(item2);
+            List<Item> result = tracker.findByName("item2");
+            assertThat(result, is(List.of(item2)));
+        }
+    }
+}
